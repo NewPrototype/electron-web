49f37359357e7ebda92ecfea14d1725b52d55798
liujunxiong <liujunxiong@quicloud.cn>
2018-06-19 20:30:57 +0800

    权限token处理，登陆默认7天有效

--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,71 @@
-import ax from 'axios'
+import ax from 'axios';
 
-import querystring from 'querystring'
+import querystring from 'querystring';
 
 import config from './../config';
 const { server } = config;
+import { message } from 'antd';
+
+
+ax.defaults.baseURL = server;
+// axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('toKen')}`;
+
+// const axConfig={
+//   headers: {'authorization': '11122'},
+//   baseURL:server,
+// }
 
 // 拦截器
-ax.interceptors.request.use(config => {
-  if (
-    config.method === 'post' ||
-    config.method === 'put' ||
-    config.method === 'delete' ||
-    config.method === 'patch'
-  ) {
-    config.data = querystring.stringify(config.data)
+ax.interceptors.request.use(
+  config => {
+    if (
+      config.method === 'post' ||
+      config.method === 'put' ||
+      config.method === 'delete' ||
+      config.method === 'patch'
+    ) {
+      config.data = querystring.stringify(config.data);
+    }
+    // console.log(`Bearer ${localStorage.getItem('toKen')}`,'---')
+    config.headers = {
+      authorization: `Bearer ${localStorage.getItem('toKen')}`,
+    };
+    // config.baseURL = server;
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
   }
-  return config
-}, error => {
-  return Promise.reject(error)
-})
+);
 
+ax.interceptors.response.use(
+  response => {
+    if (response.status) {
+      if (response.data.code == 1) {
+        message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+      return response.data.result;
+    } else {
+      message.error(response.data.message);
+    }
+    return response
+  },
+  error => {
+    message.error(error.response.data.message);
 
-ax.interceptors.response.use(response => {
-  return response
-}, error => {
-  return Promise.reject(error)
-})
-
-
+    return error.response.data
+    // return Promise.reject(error);
+  }
+);
 
 // 获取网站配置项
 export async function login(params = {}) {
-  return await ax.get(server + '/login', { params })
+  return await ax.get('/login', { params });
+}
+// 获取网站配置项
+export async function getLogin(params = {}) {
+  return await ax.get('/getlogin', { params });
 }
--- src/components/herder/Herder.js
+++ src/components/herder/Herder.js
@@ -1,29 +1,118 @@
 import React from 'react';
+import { Avatar, Modal, Input } from 'antd';
+
+import { login, getLogin } from 'api';
+import ax from 'axios'
+
+
+
 import './Herder.styl';
 
 class Herder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      user: {
+        userName: '',
+        password: '',
+      },
+      loginUser: localStorage.getItem('user')?JSON.parse(localStorage.getItem('user')) : {},
+    };
+  }
+  handleOk = () => {
+    //提交
+    const { user } = this.state;
+    login(user).then(data => {
+      localStorage.setItem('user', JSON.stringify(data.user));
+      this.setState({
+        visible: false,
+        user: {},
+        loginUser: data.user,
+      });
+      if (data.token) {
+        localStorage.setItem('toKen', data.token);
 
-constructor(props) {
-	super(props);
-	this.state = {
-	};
-}
 
-render() {
-  return (
-		<div className="herder">
-			component herder
-		</div>
-	);
-}
-	
-componentWillMount() {}
-componentDidMount() {}
-componentWillReceiveProps(nextProps) {}
-shouldComponentUpdate(nextProps, nextState) { return true; }
-componentWillUpdate(nextProps, nextState) {}
-componentDidUpdate(prevProps, prevState) {}
-componentWillUnmount() {}
+      }
+
+    });
+  };
+  render() {
+    return (
+      <div className="herder">
+        <div className="herder-left" onClick={() => {
+          getLogin().then(data => {
+            console.log(data)
+          }).catch(err => {
+            console.log(err)
+          })
+        }}>
+          <img src={require('./../../images/book.png')} alt="" /> 记账本
+        </div>
+        <div>
+          <Avatar style={{ backgroundColor: "#f56a00", verticalAlign: 'middle' }} size="large" onClick={() => {
+            this.setState({
+              visible: true,
+            })
+          }}>
+            {this.state.loginUser.userName || '未登陆'}
+          </Avatar>
+        </div>
+        <Modal
+          title="登陆中心"
+          width="300px"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={() => {
+            this.setState({ visible: false });
+          }}
+        >
+          <div className="ant-modal-content-user">
+            <div className="userName">
+              用户名:<Input
+                placeholder="请输入用户名"
+                value={this.state.user.userName}
+                onChange={e => {
+                  const { user } = this.state;
+                  user.userName = e.target.value;
+                  this.setState({
+                    user,
+                  });
+                }}
+                maxLength={10}
+              />
+            </div>
+            <div>
+              密码:<Input
+                placeholder="请输入用户密码"
+                value={this.state.user.password}
+                type="password"
+                maxLength={10}
+                onChange={e => {
+                  const { user } = this.state;
+                  user.password = e.target.value;
+                  this.setState({
+                    user,
+                  });
+                }}
+              />
+            </div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+
+  componentWillMount() { }
+  componentDidMount() { }
+  componentWillReceiveProps(nextProps) { }
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) { }
+  componentDidUpdate(prevProps, prevState) { }
+  componentWillUnmount() { }
 }
 
 export default Herder;
